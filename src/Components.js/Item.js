fdb97ec1bf48147ce2e60eaa30a090415700087b
Ivanawolf <ivanawolf.4@gmail.com>
2021-09-29 12:53:26 -0300

    promises

--- src/Components.js/Item.js
+++ src/Components.js/Item.js
@@ -3,18 +3,8 @@ import ItemCount from "./ItemCount";
 
 function Item(props) {
   const { id, nombre, precio, stock} = props.props;
-  
-//   const product = {
-//     stock: stock,
-//     initial: 1,
-//     onAdd: (stock, cantidad) => {
-//       alert(`Se agregarán ${cantidad} unidades al carrito`);
-//       return stock - cantidad;
-//     },
-//     precio: precio,
-//   };
-  
 
+  
   return (
     <div className="col-sm-6 col-md-3">
       
